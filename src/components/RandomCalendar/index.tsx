3feb2fcc80a47186da346c5838bece9d96d8b126
alessandro-silva <alessandro.rds03@gmail.com>
2020-09-15 11:47:14 -0400

    adding profile

--- src/components/RandomCalendar/index.tsx
+++ src/components/RandomCalendar/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import Heatmap from 'react-calendar-heatmap';
-import {
- subYears, isBefore, isSameDay, addDays
-} from 'date-fns';
+// eslint-disable-next-line object-curly-newline
+import { subYears, isBefore, isSameDay, addDays } from 'date-fns';
 
 import { Container } from './styles';
 
@@ -12,24 +11,21 @@ const RandomCalendar: React.FC = () => {
   const startDate = subYears(new Date(), 1);
   const endDate = new Date();
 
-  const values: HeatmapValue[] = [];
-
-  values.push({ date: new Date(), count: 3 });
-
   return (
     <Container>
       <div className="wrapper">
         <Heatmap
           startDate={startDate}
           endDate={endDate}
-          values={values}
+          // eslint-disable-next-line no-use-before-define
+          values={generateHeatmapValues(startDate, endDate)}
           gutterSize={3.5}
           classForValue={(item: HeatmapValue) => {
             let clampedCount = 0;
 
             if (item !== null) {
               clampedCount = Math.max(item.count, 0);
-              clampedCount = Math.min(item.count, 0);
+              clampedCount = Math.min(item.count, 4);
             }
 
             return `scale-${clampedCount}`;
@@ -41,6 +37,21 @@ const RandomCalendar: React.FC = () => {
       <span>Random calendar (do not represent actual data)</span>
     </Container>
   );
+};
+
+const generateHeatmapValues = (startDate: Date, endDate: Date) => {
+  const values: HeatmapValue[] = [];
+
+  let currentDate = startDate;
+  while (isBefore(currentDate, endDate) || isSameDay(currentDate, endDate)) {
+    const count = Math.random() * 4;
+
+    values.push({ date: currentDate, count: Math.round(count) });
+
+    currentDate = addDays(currentDate, 1);
+  }
+
+  return values;
 };
 
 export default RandomCalendar;
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -12,52 +12,78 @@ import {
   RightSide,
   Repos,
   CalendarHeading,
+  RepoIcon,
+  Tab,
 } from './styles';
 
-const Profile: React.FC = () => (
-  <Container>
-    <Main>
-      <LeftSide>
-        <ProfileData
-          username="alessandro-silva"
-          name="Alessandro Silva"
-          avatarUrl="https://avatars1.githubusercontent.com/u/54153011?v=4"
-          followers={887}
-          following={7}
-          company="Midascorporate"
-          location="Mato Grosso do Sul, Brazil"
-          email="alessandro.rds03@gmail.com"
-          blog="linkedin.com/in/alessandro-silva"
-        />
-      </LeftSide>
+const Profile: React.FC = () => {
+  const TabContent = () => (
+    <div className="content">
+      <RepoIcon />
+      <span className="label">Repositories</span>
+      <span className="number">26</span>
+    </div>
+  );
 
-      <RightSide>
-        <Repos>
-          <h2>Random repos</h2>
+  return (
+    <Container>
+      <Tab className="desktop">
+        <div className="wrapper">
+          <span className="offset" />
+          <TabContent />
+        </div>
 
-          <div>
-            {[1, 2, 3, 4, 5, 6].map((n) => (
-              <RepoCard
-                key={n}
-                username="alessandro-silva"
-                reponame="clone-github"
-                description="Clone do github com requisições na api do github"
-                language={n % 3 === 0 ? 'Javascript' : 'Typescript'}
-                stars={8}
-                forks={4}
-              />
-            ))}
-          </div>
-        </Repos>
+        <span className="line" />
+      </Tab>
 
-        <CalendarHeading>
-          Random calendar (do not represent actual data)
-        </CalendarHeading>
+      <Main>
+        <LeftSide>
+          <ProfileData
+            username="alessandro-silva"
+            name="Alessandro Silva"
+            avatarUrl="https://avatars1.githubusercontent.com/u/54153011?v=4"
+            followers={887}
+            following={7}
+            company="Midascorporate"
+            location="Mato Grosso do Sul, Brazil"
+            email="alessandro.rds03@gmail.com"
+            blog="linkedin.com/in/alessandro-silva"
+          />
+        </LeftSide>
 
-        <RandomCalendar />
-      </RightSide>
-    </Main>
-  </Container>
-);
+        <RightSide>
+          <Tab className="mobile">
+            <TabContent />
+            <span className="line" />
+          </Tab>
+
+          <Repos>
+            <h2>Random repos</h2>
+
+            <div>
+              {[1, 2, 3, 4, 5, 6].map(n => (
+                <RepoCard
+                  key={n}
+                  username="alessandro-silva"
+                  reponame="clone-github"
+                  description="Clone do github com requisições na api do github"
+                  language={n % 3 === 0 ? 'Javascript' : 'Typescript'}
+                  stars={8}
+                  forks={4}
+                />
+              ))}
+            </div>
+          </Repos>
+
+          <CalendarHeading>
+            Random calendar (do not represent actual data)
+          </CalendarHeading>
+
+          <RandomCalendar />
+        </RightSide>
+      </Main>
+    </Container>
+  );
+};
 
 export default Profile;
